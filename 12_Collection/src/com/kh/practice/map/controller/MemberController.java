59077ed4cdc339e54f3d8fba30fbb294d38af5f1
can-i-comein <vsseanvs@naver.com>
2025-05-09 17:54:55 +0900

    Add files via upload

--- 12_Collection/src/com/kh/practice/map/controller/MemberController.java
+++ 12_Collection/src/com/kh/practice/map/controller/MemberController.java
@@ -1,6 +1,8 @@
 package com.kh.practice.map.controller;
 
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
 import java.util.TreeMap;
 
 import com.kh.practice.map.model.vo.Member;
@@ -21,33 +23,37 @@ public class MemberController {
 	
 	public String logIn(String id, String password) {
 		if(map.containsKey(id)) {
-			Member m = map.get(id);
-			String savedPw = m.getPassword();
+//			Member m = map.get(id);
+//			String savedPw = m.getPassword();
+
+			String savedPw = map.get(id).getPassword();
 			
 			if(savedPw.equals(password))
-				return m.getName();
+				return map.get(id).getName();
+//			else
+//				return null;
 		}
 		return null;
 	}
 	
 	public boolean changePassword(String id, String oldPw, String newPw) {
 		
-		boolean result = true;
 		if(map.containsKey(id)) {
 			Member m = map.get(id);
 			String savedPw = m.getPassword();
 			if(savedPw.equals(oldPw)) {
 				m.setPassword(newPw);
+				return true;
 			} else
-				result = false;
+				return false;
 		}
-		return result;
+		return false;
 	}
 	
 	public void changeName(String id, String newName) {
 		if(map.containsKey(id)) {
-			Member m = map.get(id);
-			m.setName(newName);
+//			Member m = map.get(id);
+			map.get(id).setName(newName);
 		}
 	}
 	
@@ -55,7 +61,16 @@ public class MemberController {
 		
 		TreeMap<String, String> same = new TreeMap<String, String>();
 		
+		Set<String> set = map.keySet();
+		Iterator<String> it = set.iterator();
+		while(it.hasNext()) {
+			String key = it.next();
+			Member m = map.get(key);
+			if(m.getName().equals(name)) {
+				same.put(key, name);
+			}
+		}
 		
-		return ;
+		return same;
 	}
 }
--- 12_Collection/src/com/kh/practice/map/view/MemberMenu.java
+++ 12_Collection/src/com/kh/practice/map/view/MemberMenu.java
@@ -1,6 +1,10 @@
 package com.kh.practice.map.view;
 
+import java.util.Iterator;
+import java.util.Map.Entry;
 import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeMap;
 
 import com.kh.practice.map.controller.MemberController;
 import com.kh.practice.map.model.vo.Member;
@@ -73,63 +77,79 @@ public class MemberMenu {
 		
 	}
 	public void joinMembership() {
-		System.out.print("아이디 : ");
-		String id = sc.nextLine();
-		System.out.print("비밀번호 : ");
-		String password = sc.nextLine();
-		System.out.print("이름 : ");
-		String name = sc.nextLine();
 		
-		Member m = new Member(password, name);
-		boolean result = mc.joinMembership(id, m);
-		
-		if(result == true) {
-			System.out.println("성공적으로 회원가입을 완료했습니다.");
-		} else 
-			System.out.println("중복된 아이디입니다. 다시 입력하세요.");
+		while(true) {
+			System.out.print("아이디 : ");
+			String id = sc.nextLine();
+			System.out.print("비밀번호 : ");
+			String password = sc.nextLine();
+			System.out.print("이름 : ");
+			String name = sc.nextLine();
+			
+			Member m = new Member(password, name);
+			boolean result = mc.joinMembership(id, m);
+			
+			if(result == true) {
+				System.out.println("성공적으로 회원가입을 완료했습니다.");
+				break;
+			} else 
+				System.out.println("중복된 아이디입니다. 다시 입력하세요.");
+		}
 	}
 	public void logIn() {
-		System.out.print("아이디 : ");
-		String id = sc.nextLine();
-		System.out.print("비밀번호 : ");
-		String password = sc.nextLine();
 		
-		String result = mc.logIn(id, password);
-		if(result != null) {
-			System.out.println("환영합니다.");
-			memberMenu();
-		} else
-			System.out.println("틀린 아이디 또는 비밀번호입니다. 다시 입력해주세요.");
+		while(true) {
+			System.out.print("아이디 : ");
+			String id = sc.nextLine();
+			System.out.print("비밀번호 : ");
+			String password = sc.nextLine();
+			
+			String result = mc.logIn(id, password);
+			if(result != null) {
+				System.out.println(result + "님 환영합니다");
+				memberMenu();
+			} else
+				System.out.println("틀린 아이디 또는 비밀번호입니다. 다시 입력해주세요.");
+		}
 	}
-	public void changePassword() {
-		System.out.print("아이디 : ");
-		String id = sc.nextLine();
-		System.out.print("비밀번호 : ");
-		String password = sc.nextLine();
-		System.out.print("새로운 비밀번호 : ");
-		String newPd = sc.nextLine();
 		
-		boolean result = mc.changePassword(id, password, newPd);
-		if(result == true) {
-			System.out.println("비밀번호 변경에 성공했습니다.");
-		} else
-			System.out.println("비밀번호 변경에 실패했습니다. 다시 입력해주세요.");
+	public void changePassword() {
+		
+		while(true) {
+			System.out.print("아이디 : ");
+			String id = sc.nextLine();
+			System.out.print("비밀번호 : ");
+			String password = sc.nextLine();
+			System.out.print("새로운 비밀번호 : ");
+			String newPd = sc.nextLine();
+			
+			boolean result = mc.changePassword(id, password, newPd);
+			if(result == true) {
+				System.out.println("비밀번호 변경에 성공했습니다.");
+				break;
+			} else
+				System.out.println("비밀번호 변경에 실패했습니다. 다시 입력해주세요.");
+		}
 	}
 	public void changeName() {
-		System.out.print("아이디 : ");
-		String id = sc.nextLine();
-		System.out.print("비밀번호 : ");
-		String password = sc.nextLine();
-		String result = mc.logIn(id, password);
-		if(result == null) {
-			System.out.println("이름 변경에 실패했습니다. 다시 입력해주세요.");
-		} else {
-			System.out.println(result);
+		while(true) {
+			System.out.print("아이디 : ");
+			String id = sc.nextLine();
+			System.out.print("비밀번호 : ");
+			String password = sc.nextLine();
 			
-			System.out.print("변경할 이름 : ");
-			String newName = sc.nextLine();
-			mc.changeName(id, newName);
-			System.out.println("이름 변경에 성공하였습니다.");
+			String result = mc.logIn(id, password);
+			if(result == null) {
+				System.out.println("이름 변경에 실패했습니다. 다시 입력해주세요.");
+			} else {
+				System.out.println("현재 설정된 이름 : " + result);
+				
+				System.out.print("변경할 이름 : ");
+				String newName = sc.nextLine();
+				mc.changeName(id, newName);
+				System.out.println("이름 변경에 성공하였습니다.");
+				break;
+			}
 		}
 		
 	}
@@ -137,6 +157,14 @@ public class MemberMenu {
 		System.out.print("검색할 이름 : ");
 		String name = sc.nextLine();
 		mc.sameName(name);
+		
+		TreeMap<String, String> same = mc.sameName(name);
+		Set<Entry<String, String>> set = same.entrySet();
+		Iterator<Entry<String, String>> it = set.iterator();
+		while(it.hasNext()) {
+			Entry<String, String> me = it.next();
+			System.out.println(me.getValue() + "-" + me.getKey());
+		}
 	}
 	
 }
